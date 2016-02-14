f77cba8fab01ab837fd88fc44dbe794efbe48ad5
Wei Tang <hi@beyond.codes>
2016-02-14 12:28:47 +0800

    Add prev-ignored bin

---
+++ bin/appserver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python manage.py syncdb --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8001
---
+++ bin/worker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+celery -A legcowatch worker -l debug
